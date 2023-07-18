8ea8a7770760cf300d8c7809aa707aa01e1e45cf
Bhavi33-1 <lalaba342@gmail.com>
2023-07-18 23:18:47 +0530

    yes this is my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:newapp/mains.dart';
 
 import 'screens/home/home_screen.dart';
+import 'package:newapp/config/app_router.dart';
+import 'package:newapp/config/theme.dart';
+import 'package:newapp/screens/screens.dart';
+import 'screens/home/home_screen.dart';
+
+void main() => runApp(create());
+  // home: HomeScreen(),
+// ));
+
+// // import 'package:flutter/material.dart';
+// import 'package:newapp/config/app_router.dart';
+// import 'package:newapp/config/theme.dart';
+// import 'package:newapp/screens/screens.dart';
+// import 'screens/home/home_screen.dart';
 
 
-void main() => runApp(MaterialApp(
-  home: HomeScreen(),
-));
+class create extends StatelessWidget {
+  // const create({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bhavs Created App',
+      theme: theme(),
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      initialRoute: HomeScreen.routeName,
+      home: HomeScreen(),
+    );
+  }
+}
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: CustomAppBar(title: 'Bhavs Created App'),
+//       bottomNavigationBar: CustomNavBar(),
+//     );
+//   }
+// }
+
+// class CustomNavBar extends StatelessWidget {
+//   const CustomNavBar({
+//     super.key,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomAppBar(
+//       color: Colors.black,
+//       child: Container(
+//         height: 70,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//           IconButton(
+//               icon: Icon(Icons.home, color: Colors.white),
+//               onPressed: () {
+//             Navigator.pushNamed(context, '/');
+//           }), IconButton(
+//               icon: Icon(Icons.shopping_cart, color: Colors.white),
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/cart');
+//               }), IconButton(
+//               icon: Icon(Icons.person, color: Colors.white),
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/user');
+//               })
+//         ],),
+//       ),
+//     );
+//   }
+// }
+
+// class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
+//   const CustomAppBar({
+//     super.key,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       backgroundColor: Colors.indigo,
+//       elevation: 0,
+//       title: Container(
+//         color: Colors.white,
+//         // padding: const EdgeInsets.symmetric(
+//         //   horizontal: 20,
+//         //   vertical: 10,
+//         // ),
+//         child: Text('Bhavs Created App',
+//           style: TextStyle(
+//               color: Colors.black,
+//               fontSize: 24,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
+//       iconTheme: IconThemeData(color: Colors.white),
+//       actions: [IconButton(onPressed: () {}, icon: Icon(Icons.favorite))],
+//     );
+//   }
+//
+//   @override
+//   // TODO: implement preferredSize
+//   Size get preferredSize => Size.fromHeight(50.0);
+// }
+
 
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:newapp/widgets/widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:newapp/models/category_model.dart';
-import 'package:flutter/material.dart'hide Element;
+// import 'package:flutter/material.dart'hide Element;
 import 'package:newapp/models/modals.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  // const HomeScreen({Key? key}) : super(key: key);
 static const String routeName = '/home';
 
 static Route route() {
   return MaterialPageRoute(
     settings: RouteSettings(name: routeName),
-    builder: (context) => HomeScreen(),
+    builder: (_) => HomeScreen(),
   );
 }
   @override
@@ -24,17 +24,17 @@ static Route route() {
       bottomNavigationBar: CustomNavBar(),
       body: Column(
       children: [
-        // CarouselSlider(
-        //   options: CarouselOptions(
-        //     aspectRatio: 1.5,
-        //     viewportFraction: 0.9,
-        //     enlargeCenterPage: true,
-        //     enlargeStrategy: CenterPageEnlargeStrategy.height,
-        //   ),
-        //   items: Category.categories
-        //       .map((category) => HeroCarouselCard(category: category))
-        //       .toList(),
-        // ),
+        CarouselSlider(
+          options: CarouselOptions(
+            aspectRatio: 1.5,
+            viewportFraction: 0.9,
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+          ),
+          items: Category.categories
+              .map((category) => HeroCarouselCard(category: category))
+              .toList(),
+        ),
         SectionTitle(title: 'Recommended'),
         ProductCarousel(products: Product.products
             .where((product) => product.isRecommended)
